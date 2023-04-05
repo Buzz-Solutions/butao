d4af0b8a5062be089ae1a0a8843f59189628bc87
David Grayson <david.grayson@buzzsolutions.co>
2023-04-05 15:27:05 -0700

    make install work on mac

--- install_tao.sh
+++ install_tao.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
 
 version=$1
 
 echo "Downloading the getting started package..."
-wget --content-disposition "https://api.ngc.nvidia.com/v2/resources/nvidia/tao/tao-getting-started/versions/${version}/zip" -O "getting_started_v${version}.zip"
+
+url="https://api.ngc.nvidia.com/v2/resources/nvidia/tao/tao-getting-started/versions/${version}/zip"
+outfile="getting_started_v${version}.zip"
+
+if [[ "$OS" == "Darwin" ]]; then
+    curl -J -L $url -o $outfile
+else
+    wget --content-disposition $url -O $outfile
+fi
 
 echo "Extracting the getting started package..."
-unzip -u "getting_started_v${version}.zip" -d "./getting_started_v${version}" && rm -rf "getting_started_v${version}.zip"
+unzip -u $outfile -d "./getting_started_v${version}" && rm -f $outfile
 
 echo "Installing the getting started package..."
 bash getting_started_v${version}/setup/quickstart_launcher.sh --install
